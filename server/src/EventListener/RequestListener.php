d0127a2634887a117ede69091acc3a36a15c29f1
CommandMaker <command.maker83@gmail.com>
2024-09-02 15:32:08 +0200

    feat(api): protect routes with password

---
+++ server/src/EventListener/RequestListener.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Exception\PasswordMissingException;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+
+final class RequestListener
+{
+    public function __construct(
+        private string $masterPassword,
+    ) {}
+
+    #[AsEventListener(event: KernelEvents::REQUEST)]
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if ($request->getMethod() !== 'GET' && $request->getRequestUri() !== '/api/card_scans') {
+            $password = $request->getPayload()->getString('master_password');
+
+            if ($password === '' && $request->query->getString('master_password') === '') {
+                throw new PasswordMissingException('This route needs a password to be accessed');
+            }
+
+            if ($request->query->getString('master_password') !== '') {
+                $password = $request->query->getString('master_password');
+            }
+
+            $factory = new PasswordHasherFactory([
+                'default' => ['algorithm' => 'auto'],
+            ]);
+
+            $hasher = $factory->getPasswordHasher('default');
+
+            if (!$hasher->verify($this->masterPassword, $password)) {
+                throw new BadCredentialsException('The password is incorrect');
+            }
+        }
+    }
+}

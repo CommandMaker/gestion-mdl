6c92e0cb968fc9a284d3817ea08eee6a3059dd2d
CommandMaker <command.maker83@gmail.com>
2024-09-06 12:49:23 +0200

    feat(api): login

---
+++ server/src/Controller/APIJsonLoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class APIJsonLoginController extends AbstractController
+{
+    #[Route('/login', name: 'login.json', methods: 'POST')]
+    public function index(): JsonResponse
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/api/logout', name: 'login.logout')]
+    public function logout(): void
+    {
+    }
+}
